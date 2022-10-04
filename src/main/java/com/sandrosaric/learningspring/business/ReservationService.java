91bd62349a6b3da7a1187c2830c3c1e45a5d6455
Sandro Saric <sandro.saric@gmail.com>
2022-10-04 15:23:51 +0200

    Added MVC

--- src/main/java/com/sandrosaric/learningspring/business/ReservationService.java
+++ src/main/java/com/sandrosaric/learningspring/business/ReservationService.java
@@ -58,5 +58,23 @@ public class ReservationService {
         });
         return roomReservations;
     }
+
+    public List<Guest> getHotelGuests(){
+        Iterable<Guest> guests = guestRepository.findAll();
+        List<Guest> guestList = new ArrayList<>();
+        guests.forEach(g -> guestList.add(g));
+        guestList.sort(new Comparator<Guest>() {
+            @Override
+            public int compare(Guest o1, Guest o2) {
+                if(o1.getLastName().equals(o2.getLastName())){
+                    return o1.getFirstName().compareTo(o2.getFirstName());
+                }
+                return o1.getLastName().compareTo(o2.getLastName());
+            }
+        });
+
+
+        return guestList;
+}
 }
 
---
+++ src/main/java/com/sandrosaric/learningspring/web/RoomReservationController.java
@@ -0,0 +1,35 @@
+package com.sandrosaric.learningspring.web;
+
+
+import com.sandrosaric.learningspring.business.ReservationService;
+import com.sandrosaric.learningspring.business.RoomReservation;
+import com.sandrosaric.learningspring.util.DateUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@RequestMapping("/reservations")
+public class RoomReservationController {
+    private final DateUtils dateUtils;
+    private final ReservationService reservationService;
+
+    public RoomReservationController(DateUtils dateUtils, ReservationService reservationService) {
+        this.dateUtils = dateUtils;
+        this.reservationService = reservationService;
+    }
+
+    @RequestMapping(method= RequestMethod.GET)
+    public String getReservations(@RequestParam(value="date",required = false) String dateString, Model model){
+        Date date = this.dateUtils.createDateFromDateString(dateString);
+        List<RoomReservation> roomReservations = this.reservationService.getRoomReservationsForDate(date);
+        model.addAttribute("roomReservations",roomReservations);
+        return "roomres";
+    }
+
+}
